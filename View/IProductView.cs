2d0ca394c173e87c6e9d7af2e674b3ca1e9621be
artem <artem@PA77>
2025-05-25 14:53:41 +0300

    Сделал начальную структуру для mvp шаблона

---
+++ View/IProductView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductsShop.Model;
+
+namespace ProductsShop.View
+{
+    public interface IProductView
+    {
+        void DisplayProducts(List<Product> products);
+        
+        event EventHandler AddProductRequested;
+        event EventHandler DeleteProductRequested;
+        event EventHandler SaveDataInFile;
+        event EventHandler ReadDataFromFile;
+        void ShowMessage(string message);
+        void ShowError(string message);
+        
+    }
+}
---
+++ View/MainForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProductsShop.Model;
+using ProductsShop.View;
+
+namespace ProductsShop
+{
+    public partial class MainForm : Form, IProductView
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        public event EventHandler AddProductRequested;
+        public event EventHandler SaveDataInFile;
+        public event EventHandler ReadDataFromFile;
+        public event EventHandler DeleteProductRequested;
+        public void DisplayProducts(List<Product> products)
+        {
+            listViewProducts.Items.Clear();
+            foreach (var product in products)
+            {
+                ListViewItem item = new ListViewItem(product.Id.ToString());
+                item.SubItems.Add(product.Name);
+                item.SubItems.Add(product.Price.ToString() + " руб.");
+                listViewProducts.Items.Add(item);
+            }
+        }
+        public void ShowMessage(string message)
+        {
+            MessageBox.Show(message, "Успешно", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        public void ShowError(string message)
+        {
+            MessageBox.Show(message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
